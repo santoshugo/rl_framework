136036449ed4053d87364db578e8e32634d9386b
Hugo <santos.hugomb@gmail.com>
2020-11-09 17:30:37 +0000

    ma rl env

--- projects/dummy_project_2/rllib_poc/run_env.py
+++ projects/dummy_project_2/rllib_poc/run_env.py
@@ -1,55 +1,42 @@
 import argparse
-from gym.spaces import Tuple, MultiDiscrete, Dict, Discrete
 import os
-
+from gym.spaces import Discrete, MultiDiscrete
 import ray
-from ray import tune
-from ray.tune import register_env, grid_search
-from ray.rllib.env.multi_agent_env import ENV_STATE
-from ray.rllib.examples.env.two_step_game import TwoStepGame
-from ray.rllib.utils.test_utils import check_learning_achieved
+from ray.tune import register_env
+from ray.rllib.agents import dqn, pg
+
+from ray.tune.logger import pretty_print
 
 from projects.dummy_project_2.rllib_poc.simple_env import SimpleEnvironment
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--run", type=str, default="PG")
-parser.add_argument("--num-cpus", type=int, default=0)
-# parser.add_argument("--as-test", action="store_true")
-parser.add_argument("--torch", action="store_true")
+parser.add_argument("--as-test", action="store_true")
 parser.add_argument("--stop-reward", type=float, default=7.0)
-parser.add_argument("--stop-timesteps", type=int, default=500)
+parser.add_argument("--stop-timesteps", type=int, default=50000)
 
 
 if __name__ == '__main__':
-    args = parser.parse_args()
 
     register_env(
         "simple_env",
         lambda config: SimpleEnvironment(config)
     )
 
-    config = {
-        # Use GPUs iff `RLLIB_NUM_GPUS` env var set to > 0.
-        "num_gpus": int(os.environ.get("RLLIB_NUM_GPUS", "0")),
-        "framework": "tf",
-    }
-    group = False
+    obs_space = MultiDiscrete([5, 2])
+    act_space = Discrete(7)
 
-    ray.init(_node_ip_address="127.0.0.1")
-
-    stop = {
-        "episode_reward_mean": args.stop_reward,
-        "timesteps_total": args.stop_timesteps,
-    }
-
-    config = dict(config, **{
-        "env": "simple_env",
+    trainer = dqn.DQNTrainer(env="simple_env", config={
+        "multiagent": {
+            "policies": {
+                # the first tuple value is None -> uses default policy
+                'pol1': (None, obs_space, act_space, {"gamma": 1}),
+                'pol2': (None, obs_space, act_space, {"gamma": 1})
+            },
+            "policy_mapping_fn": lambda agent_id: 'pol1' if agent_id == 1 else 'pol2'
+        },
     })
 
-    results = tune.run(args.run, stop=stop, config=config, verbose=1)
-
-    if args.as_test:
-        check_learning_achieved(results, args.stop_reward)
-
-    ray.shutdown()
+    for i in range(100):
+        print(trainer.train())
--- projects/dummy_project_2/rllib_poc/simple_env.py
+++ projects/dummy_project_2/rllib_poc/simple_env.py
@@ -1,4 +1,4 @@
-from gym.spaces import Discrete
+from gym.spaces import Discrete, MultiDiscrete
 import json
 import numpy as np
 import networkx as nx
@@ -7,91 +7,121 @@ from ray.rllib.env.multi_agent_env import MultiAgentEnv, ENV_STATE
 
 
 class SimpleEnvironment(MultiAgentEnv):
-    PICKUP = 6
-    DROPDOWN = 7
-    MOVEMENT = (1, 2, 3, 4, 5)
+    PICKUP = 5
+    DROPDOWN = 6
+    MOVEMENT = (0, 1, 2, 3, 4)
     """
     Simple graph environment of the form
 
-    3 -- 4 -- 5
+    2 -- 3 -- 4
         | |
-    1 --| | -- 2
+    0 --| | -- 1
 
     Agents 1 starts in 1 and goes to 3 (and back), and agent 2 starts in 2 and goes to 5 (and back). Must pickup in 3/5 and dropdown in 1/2
     """
     def __init__(self, config):
         self.graph = nx.Graph()
-        self.graph.add_nodes_from([1, 2, 3, 4, 5])
-        self.graph.add_edges_from([(1, 4), (2, 4), (3, 4), (4, 5)])
+        self.graph.add_nodes_from([0, 1, 2, 3, 4])
+        self.graph.add_edges_from([(0, 3), (1, 3), (2, 3), (4, 3)])
 
-        self.state = {1: 1, 2: 2}
-        self.carrying = {1: False, 2: False}
+        self.state = {0: 0, 1: 1}
+        self.carrying = {0: False, 1: False}
 
-        self.action_space = Discrete(5 + 2)
-        self.observation_space = Discrete(5 * 2)  # ?
+        self.action_space = Discrete(7)
+        self.observation_space = MultiDiscrete([5, 2])
         self.num_moves = 0
+
+        self.done0 = False
+        self.done1 = False
 
     def reset(self):
-        self.num_moves = 0
-        self.state = {1: 1, 2: 2}
+        self.state = {0: 0, 1: 1}
+        self.carrying = {0: False, 1: False}
+        self.done0 = False
+        self.done1 = False
+
+        return {0: self._obs(0), 1: self._obs(1)}
 
     def step(self, action_dict):
-        state1, action1 = self.state[1], action_dict[1]
-        state2, action2 = self.state[2], action_dict[2]
+        rew = {}
+        obs = {}
+        for agent, action in action_dict.items():
+            if agent == 0:
+                r = self.act_0(action)
+            else:
+                r = self.act_1(action)
+
+            rew[agent] = r
+            obs[agent] = self._obs(agent)
+
+        self.num_moves += 1
+        done = {0: self.done0, 1: self.done1, "__all__": all([self.done0, self.done1])}
+        # print(action_dict, self.state, self.carrying, rew, done)
+        return obs, rew, done, {}
+
+    def act_0(self, action0):
+        state0 = self.state[0]
+
+        # movement cases
+        if action0 not in self.graph.adj[state0] and action0 not in [self.PICKUP, self.DROPDOWN]:
+            r0 = -1
+        elif action0 in self.graph.adj[state0]:
+            self.state[0] = action0
+            r0 = -0.1
+        # pickup cases
+        elif action0 == 5:
+            if state0 != 2:
+                r0 = -1
+            elif state0 == 2 and not self.carrying[0]:
+                self.carrying[0] = True
+                r0 = 1
+            elif state0 == 2 and self.carrying[0]:
+                r0 = -1
+        # dropdown cases
+        elif action0 == 6:
+            if state0 != 0:
+                r0 = -1
+            elif state0 == 0 and self.carrying[0]:
+                self.done0 = True
+                self.carrying[0] = False
+                r0 = 1
+            elif state0 == 0 and not self.carrying[0]:
+                r0 = -1
+
+        return r0
+
+    def act_1(self, action1):
+        state1 = self.state[1]
 
         # movement cases
         if action1 not in self.graph.adj[state1] and action1 not in [self.PICKUP, self.DROPDOWN]:
             r1 = -1
         elif action1 in self.graph.adj[state1]:
             self.state[1] = action1
-            r1 = 0
+            r1 = -0.1
         # pickup cases
-        elif action1 == 6:
-            if state1 != 3:
+        elif action1 == 5:
+            if state1 != 4:
                 r1 = -1
-            elif state1 == 3 and not self.carrying[1]:
+            elif state1 == 4 and not self.carrying[1]:
                 self.carrying[1] = True
                 r1 = 1
-            elif state1 == 3 and self.carrying[1]:
+            elif state1 == 4 and self.carrying[1]:
                 r1 = -1
         # dropdown cases
-        elif action1 == 7:
+        elif action1 == 6:
             if state1 != 1:
                 r1 = -1
             elif state1 == 1 and self.carrying[1]:
+                self.done1 = True
                 self.carrying[1] = False
                 r1 = 1
             elif state1 == 1 and not self.carrying[1]:
                 r1 = -1
 
-        # movement cases
-        if action2 not in self.graph.adj[state2] and action2 not in [self.PICKUP, self.DROPDOWN]:
-            r2 = -1
-        elif action2 in self.graph.adj[state2]:
-            self.state[2] = action2
-            r2 = 0
-        # pickup cases
-        elif action2 == 6:
-            if state2 != 3:
-                r2 = -1
-            elif state2 == 5 and not self.carrying[2]:
-                self.carrying[2] = True
-                r2 = 1
-            elif state2 == 5 and self.carrying[2]:
-                r2 = -1
-        # dropdown cases
-        elif action2 == 7:
-            if state2 != 2:
-                r2 = -1
-            elif state2 == 2 and self.carrying[2]:
-                self.carrying[2] = False
-                r2 = 1
-            elif state2 == 2 and not self.carrying[2]:
-                r2 = -1
+        return r1
 
-        self.num_moves += 1
-        done = {"__all__": self.num_moves >= 50}
-
-        return self.state, {1: r1, 2: r2}, done, {}
+    def _obs(self, agent):
+        return np.array([self.state[agent], self.carrying[agent]])
 
 
